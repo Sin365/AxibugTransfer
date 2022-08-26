0b38c0ab17238d0f229f6ab2d60fc38e4f3fa139
sin365 <353374337@qq.com>
2022-08-26 16:38:41 +0800

    控制台基本版本

---
+++ AxibugTransfer.Config/Config.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace AxibugTransfer.Config
+{
+    public struct CfgInfo
+    {
+        public int localPort;
+        public string targetIP;
+        public int targetPort;
+    }
+
+    public static class Config
+    {
+        public static bool LoadConfig(out List<CfgInfo> cfgList)
+        {
+            cfgList = new List<CfgInfo>();
+            try
+            {
+                StreamReader sr = new StreamReader(System.Environment.CurrentDirectory + "\\config.cfg", Encoding.Default);
+                String line;
+                while (!string.IsNullOrEmpty((line = sr.ReadLine())))
+                {
+                    if (!line.Contains(":"))
+                        continue;
+                    try
+                    {
+                        CfgInfo cfg = new CfgInfo()
+                        {
+                            localPort = Convert.ToInt32(line.Split(':')[0].Trim()),
+                            targetIP = line.Split(':')[1].Trim(),
+                            targetPort = Convert.ToInt32(line.Split(':')[2].Trim())
+                        };
+                        cfgList.Add(cfg);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+                sr.Close();
+
+                if (cfgList.Count > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("配置文件异常："+ex.ToString());
+                return false;
+            }
+        }
+
+    }
+}
---
+++ AxibugTransfer/Program.cs
@@ -0,0 +1,14 @@
+﻿using AxibugTransfer.Tcp;
+using AxibugTransfer.Config;
+
+if (!Config.LoadConfig(out List<CfgInfo> cfgList))
+{
+    Console.WriteLine("请检查配置文件!");
+    return;
+}
+
+foreach (CfgInfo cfg in cfgList)
+    new TcpTransfer(cfg.localPort, cfg.targetIP, cfg.targetPort, true).Start();
+
+while (true)
+    Console.ReadLine();
