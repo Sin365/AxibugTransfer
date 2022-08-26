d88496fd412f791ff78566e8eb1edbb154589d9f
sin365 <353374337@qq.com>
2022-08-26 17:04:16 +0800

    0.1

--- AxibugTransfer/Program.cs
+++ AxibugTransfer/Program.cs
@@ -1,9 +1,13 @@
 ﻿using AxibugTransfer.Tcp;
 using AxibugTransfer.Config;
 
+
+Console.WriteLine("AxibugTransfer Ver.0.1  By 皓月");
+
 if (!Config.LoadConfig(out List<CfgInfo> cfgList))
 {
     Console.WriteLine("请检查配置文件!");
+    Console.ReadLine();
     return;
 }
 
